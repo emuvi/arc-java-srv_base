cb1d6085299212e28064535a3f89e61f8867561d
Éverton M. Vieira <emuvi@outlook.com.br>
2022-05-23 09:18:32 -0300

    initial version

---
+++ src/main/java/br/net/pin/srv_base/SrvBase.java
@@ -0,0 +1,13 @@
+package br.net.pin.srv_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrvBase {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrvBase.class, args);
+	}
+
+}
---
+++ src/test/java/br/net/pin/srv_base/SrvBaseTest.java
@@ -0,0 +1,13 @@
+package br.net.pin.srv_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrvBaseTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
